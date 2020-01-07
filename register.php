fd5f2c5a3689e0fdd8565df1e01df3c4085f15b5
alexpich <pich.alex01@gmail.com>
2020-01-06 21:33:59 -0800

    Added event to hide and show forms

--- register.php
+++ register.php
@@ -23,10 +23,14 @@ function getInputValue($name)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Soundify</title>
+
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
 </head>
 
 <body>
