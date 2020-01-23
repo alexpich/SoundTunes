9bde39c4ccac6f41e50ad782dae91598d8765651
alexpich <pich.alex01@gmail.com>
2020-01-22 21:23:23 -0800

    Added the Artist class

--- album.php
+++ album.php
@@ -9,7 +9,10 @@ if (isset($_GET['id'])) {
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
-echo $album['title'];
+$artist = new Artist($con, $album['artist']);
+
+echo $album['title'] . "<br/>";
+echo $artist->getName();
 
 ?>
 
